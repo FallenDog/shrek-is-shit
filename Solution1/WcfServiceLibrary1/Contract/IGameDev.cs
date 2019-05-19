f566fb4cec819bec296f4740d8ff99b8e4f4c8b9
user <user@A24PC3>
2019-05-19 12:28:20 +0300

    asd

---
+++ Solution1/WcfServiceLibrary1/Contract/IGameDev.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary1.Contract
+{
+
+    [ServiceContract]
+    interface IGameDev
+    {
+        [OperationContract]
+        void AddGame();
+        [OperationContract]
+        void DelGame();
+
+    }
+}
---
+++ Solution1/WcfServiceLibrary1/Servise/GameDev.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary1.Contract;
+
+namespace WcfServiceLibrary1.Servise
+{
+    class GameDev : IGameDev
+    {
+        public void AddGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DelGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
